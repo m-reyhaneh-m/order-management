66fb82c5df2df18eace750f79762232d9da8e53a
Reyhaneh Hasanzadeh <hasanzadereihane866@gmail.com>
2026-08-26 10:30:48 +0330

    add project pages and routing and bootstrap

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link className="nav-link" to="/dashboard">
+        Dashboard
+      </Link>
+      <Link className="nav-link" to="/orders">
+        Orders
+      </Link>
+      <Link className="nav-link" to="/orders/new">
+        AddOrder
+      </Link>
+      <Link className="nav-link" to="/orders/:id">
+        OrderDetails
+      </Link>
+    </nav>
+  );
+}
