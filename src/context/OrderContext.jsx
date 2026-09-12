abfb21041ed616238b5d479e009057c45b19770f
Reyhaneh Hasanzadeh <hasanzadereihane866@gmail.com>
2026-09-12 12:05:37 +0330

    move order fetching to context

--- src/context/OrderContext.jsx
+++ src/context/OrderContext.jsx
@@ -1,13 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getOrder } from "../services/orderApi";
 
 const OrderContext = createContext();
 function OrderProvider({ children }) {
-    const [orders, setOrders] = useState([]);
-    return(
-        <OrderContext.Provider value={{orders, setOrders}}>
-            {children}
-        </OrderContext.Provider>
-    )
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const loadOrder = async () => {
+      try {
+        const data = await getOrder();
+        setOrders(data);
+      } catch (error) {
+        console.log("error: ", error);
+        setError("!دریافت سفارش ها با خطا مواجه شد");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadOrder();
+  }, []);
+
+  return (
+    <OrderContext.Provider value={{ orders, setOrders, isLoading, error }}>
+      {children}
+    </OrderContext.Provider>
+  );
 }
 
 export { OrderContext, OrderProvider };
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,28 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import { getOrder } from "../services/orderApi";
+import React, { useContext} from "react";
 import OrderCard from "../components/OrderCard";
 import Loading from "../Loading/Loading";
 import { OrderContext } from "../context/OrderContext";
 
 export default function Orders() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const {orders , setOrders} = useContext(OrderContext)
-  useEffect(() => {
-    const loadOrder = async () => {
-      try {
-        const data = await getOrder();
-        setOrders(data);
-      } catch (error) {
-        console.log("error: ", error);
-        setError("!دریافت سفارش ها با خطا مواجه شد");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    loadOrder();
-  }, []);
+  const { orders, isLoading, error } = useContext(OrderContext);
   if (error) {
     return <p className="text-danger fw-bold fs-5 text-center my-5">{error}</p>;
   } else {
